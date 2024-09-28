cfc78aef12bec67e5a9aa92f33e57b2268795234
박승일 <psi9730@gmail.com>
2024-09-28 22:41:11 +0900

    feat: first project setting

---
+++ src/main/kotlin/com/example/review_pannel/ReviewPannelApplication.kt
@@ -0,0 +1,11 @@
+package com.example.review_pannel
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewPannelApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReviewPannelApplication>(*args)
+}
